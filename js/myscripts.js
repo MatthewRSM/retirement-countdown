3536075b485c12b17516c07333dab678f1ad69ab
MatthewRSM <140020490+MatthewRSM@users.noreply.github.com>
2023-12-21 23:18:57 -0500

    IT WORKS!
    Mobile viewport to be added

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date('2024, 12, 31').getTime();
+var countDownDate = 1735707600000;
 
 // Update the count down every 1 second
 var x = setInterval(function() {
