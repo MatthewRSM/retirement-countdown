d88d08e2c0ee08f453be61718bc865040e983ea9
MatthewRSM <140020490+MatthewRSM@users.noreply.github.com>
2023-12-25 00:10:12 -0500

    Hopefully final changes
    Added second(s) functionality, changed background.

--- js/myscripts.js
+++ js/myscripts.js
@@ -15,9 +15,58 @@ var x = setInterval(function() {
     var hours = Math.floor((distance % (86400000)) / (3600000));
     var minutes = Math.floor((distance % (3600000)) / (60000));
     var seconds = Math.floor((distance % (60000)) / 1000);
-        
+
+    var daysStr = " days<br>";
+    var hoursStr = " hours<br>";
+    var minutesStr = " minutes<br>";
+    var secondsStr = " seconds<br>";
+
+    if (days == 1) {
+
+        daysStr = " day<br>";
+
+    }
+    else {
+
+        daysStr = " days<br>";
+
+    } 
+
+    if (hours == 1) {
+
+        hoursStr = " hour<br>";
+
+    }
+    else {
+
+        hoursStr = " hours<br>";
+
+    } 
+
+    if (minutes == 1) {
+
+        minutesStr = " minute<br>";
+
+    }
+    else {
+
+        minutesStr = " minutes<br>";
+
+    } 
+
+    if (seconds == 1) {
+
+        secondsStr = " second<br>";
+
+    }
+    else {
+
+        secondsStr = " seconds<br>";
+
+    } 
+
     // Output the result in an element with id="countdown"
-    document.getElementById("countdown").innerHTML = days + " days<br>" + hours + " hours<br>" + minutes + " minutes<br>" + seconds + " seconds<br>";
+    document.getElementById("countdown").innerHTML = days + daysStr + hours + hoursStr + minutes + minutesStr + seconds + secondsStr;
         
     // If the count down is over, write some text 
     if (distance < 0) {
